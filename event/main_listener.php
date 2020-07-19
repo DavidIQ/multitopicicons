f8ccc49ebc0ec75e9c084f234ac2329c3965235a
David Colón <david@davidiq.com>
2020-07-18 21:21:11 -0400

    Display icons in viewtopic

--- event/main_listener.php
+++ event/main_listener.php
@@ -13,6 +13,7 @@ namespace davidiq\multitopicicons\event;
 /**
  * @ignore
  */
+
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -20,142 +21,118 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class main_listener implements EventSubscriberInterface
 {
-	public static function getSubscribedEvents()
-	{
-		return [
-			'core.posting_modify_template_vars'			=> 'load_topic_icons',
-            'core.posting_modify_submission_errors'     => 'check_max_limit',
-            'core.submit_post_end'                      => 'save_topic_icons',
-		];
-	}
+    public static function getSubscribedEvents()
+    {
+        return [
+            'core.posting_modify_template_vars' => 'load_topic_icons',
+            'core.posting_modify_submission_errors' => 'check_max_limit',
+            'core.submit_post_end' => 'save_topic_icons',
+            'core.viewtopic_modify_post_row' => 'add_topic_icons',
+        ];
+    }
 
-	/* @var \phpbb\language\language */
-	protected $language;
+    /* @var \phpbb\language\language */
+    protected $language;
 
-	/* @var \phpbb\template\template */
-	protected $template;
-
-	/* @var \phpbb\db\driver\driver_interface */
-	protected $db;
-
-	/* @var string */
-	protected $topic_icons_table;
-
-	/* @var \phpbb\request\request */
-    protected $request;
-
-    /* @var \phpbb\cache\service */
-    protected $cache;
-
-    /* @var \phpbb\config\config */
-    protected $config;
+    /* @var \phpbb\template\template */
+    protected $template;
 
     /* @var string */
-    protected $root_path;
+    protected $topic_icons_table;
+
+    /* @var \phpbb\request\request */
+    protected $request;
+
+    /* @var \davidiq\multitopicicons\service */
+    protected $service;
 
     /**
      * Constructor
      *
      * @param \phpbb\language\language $language Language object
      * @param \phpbb\template\template $template Template object
-     * @param \phpbb\db\driver\driver_interface $db dbal interaction
      * @param \phpbb\request\request $request Request object
-     * @param \phpbb\cache\service $cache Cache object
-     * @param \phpbb\config\config $config Config object
-     * @param string $root_path Root path
+     * @param \davidiq\multitopicicons\service $service Multi topic icons service
      * @param string $topic_icons_table Topic icons table name
      */
-	public function __construct(\phpbb\language\language $language, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\cache\service $cache, \phpbb\config\config $config, string $root_path, string $topic_icons_table)
-	{
-		$this->language = $language;
-		$this->template = $template;
-		$this->db = $db;
-		$this->topic_icons_table = $topic_icons_table;
-		$this->request = $request;
-		$this->cache = $cache;
-		$this->config = $config;
-		$this->root_path = $root_path;
-	}
+    public function __construct(\phpbb\language\language $language, \phpbb\template\template $template, \phpbb\request\request $request, \davidiq\multitopicicons\service $service, string $topic_icons_table)
+    {
+        $this->language = $language;
+        $this->topic_icons_table = $topic_icons_table;
+        $this->request = $request;
+        $this->template = $template;
+        $this->service = $service;
+    }
 
-	/**
-	 * Load multi topic icons on posting page
-	 *
-	 * @param \phpbb\event\data	$event	Event object
-	 */
-	public function load_topic_icons($event)
-	{
-	    $s_topic_icons = $event['s_topic_icons'];
-	    if ($s_topic_icons)
+    /**
+     * Load multi topic icons on posting page
+     *
+     * @param \phpbb\event\data $event Event object
+     */
+    public function load_topic_icons($event)
+    {
+        $s_new_message = $this->template->retrieve_var('S_NEW_MESSAGE');
+        $s_topic_icons = $event['s_topic_icons'];
+        if ($s_topic_icons && $s_new_message)
         {
-    	    $page_data = $event['page_data'];
+            $page_data = $event['page_data'];
 
             $this->language->add_lang('posting', 'davidiq/multitopicicons');
             $page_data['S_SHOW_TOPIC_ICONS'] = false;
             $page_data['S_SHOW_MULTI_TOPIC_ICONS'] = true;
 
-            $topic_icons = [];
+            $topic_id = (int)$event['topic_id'];
             $icon_id = 0;
 
-            $topic_id = (int) $event['topic_id'];
             if ($topic_id)
             {
                 $post_data = $event['post_data'];
-                $icon_id = (int) $post_data['icon_id'];
-
-                $sql = "SELECT icon_id FROM {$this->topic_icons_table} WHERE topic_id = $topic_id";
-                $result = $this->db->sql_query($sql);
-                $topic_icons = $this->db->sql_fetchrowset($result);
-                $this->db->sql_freeresult($result);
+                $icon_id = (int)$post_data['icon_id'];
             }
 
-            $root_path = (defined('PHPBB_USE_BOARD_URL_PATH') && PHPBB_USE_BOARD_URL_PATH) ? generate_board_url() . '/' : $this->root_path;
-            $icons = $this->cache->obtain_icons();
-            foreach ($icons as $id => $data)
-            {
-                if ($data['display'])
-                {
-                    $topic_icon = array_filter($topic_icons, function($icon) use ($id) {
-                        return $icon['icon_id'] == $id;
-                    });
-                    $this->template->assign_block_vars('topic_icons', [
-                        'ICON_ID'		=> $id,
-                        'ICON_IMG'		=> $root_path . $this->config['icons_path'] . '/' . $data['img'],
-                        'ICON_WIDTH'	=> $data['width'],
-                        'ICON_HEIGHT'	=> $data['height'],
-                        'ICON_ALT'		=> $data['alt'],
-                        'S_CHECKED'		=> !empty($topic_icon) || $id === $icon_id,
-                    ]);
-                }
-            }
-
+            $this->service->assign_icons_to_template($topic_id, $icon_id);
             $event['page_data'] = $page_data;
         }
-	}
+    }
 
-	public function check_max_limit($event)
+    /**
+     * Check that the max limit of post icons is respected
+     *
+     * @param \phpbb\event\data $event Event object
+     */
+    public function check_max_limit($event)
     {
         // Check for max allowed icons
         $post_data = $event['post_data'];
     }
 
+    /**
+     * Save checked topic icons
+     *
+     * @param \phpbb\event\data $event Event object
+     */
     public function save_topic_icons($event)
     {
-        $topic_id = (int) $event['data']['topic_id'];
-        $sql = "DELETE FROM {$this->topic_icons_table} WHERE topic_id = $topic_id";
-        $this->db->sql_query($sql);
-
-        $sql_ary = [];
+        $topic_id = (int)$event['data']['topic_id'];
         $topic_icons = $this->request->variable('topic_icons', [0]);
-        foreach ($topic_icons as $icon_id)
+        $this->service->save_topic_icons($topic_id, $topic_icons);
+    }
+
+    /**
+     * Display topic icons in viewtopic
+     *
+     * @param \phpbb\event\data $event Event object
+     */
+    public function add_topic_icons($event)
+    {
+        $post_row = $event['post_row'];
+        if ($post_row['S_FIRST_POST'] && !empty($post_row['POST_ICON_IMG']))
         {
-            $sql_ary[] = [
-				'topic_id'	=> $topic_id,
-				'icon_id'	=> (int) $icon_id,
-			];
-        }
-        if (count($sql_ary))
-        {
-            $this->db->sql_multi_insert($this->topic_icons_table, $sql_ary);
+            $post_row['POST_ICON_IMG'] = false;
+            $event['post_row'] = $post_row;
+            $row = $event['row'];
+            $topic_data = $event['topic_data'];
+            $this->service->assign_icons_to_template($topic_data['topic_id'], $row['icon_id']);
         }
     }
 }
--- service.php
+++ service.php
@@ -15,32 +15,99 @@ namespace davidiq\multitopicicons;
  */
 class service
 {
-	/** @var \phpbb\user */
-	protected $user;
+    /* @var \phpbb\cache\service */
+    protected $cache;
 
-	/** @var string */
-	protected $table_name;
+    /* @var \phpbb\config\config */
+    protected $config;
 
-	/**
-	 * Constructor
-	 *
-	 * @param \phpbb\user $user       User object
-	 * @param string      $table_name The name of a db table
-	 */
-	public function __construct(\phpbb\user $user, $table_name)
-	{
-		$this->user = $user;
-		$this->table_name = $table_name;
-	}
+    /* @var \phpbb\template\template */
+    protected $template;
 
-	/**
-	 * Get user object
-	 *
-	 * @return \phpbb\user $user User object
-	 */
-	public function get_user()
-	{
-		var_dump($this->table_name);
-		return $this->user;
-	}
+    /* @var \phpbb\db\driver\driver_interface */
+    protected $db;
+
+    /* @var string */
+    protected $root_path;
+
+    /** @var string */
+    protected $topic_icons_table;
+
+    /**
+     * Constructor
+     *
+     * @param \phpbb\cache\service $cache Cache object
+     * @param \phpbb\config\config $config
+     * @param \phpbb\template\template $template
+     * @param \phpbb\db\driver\driver_interface $db
+     * @param string $root_path Root path of forum
+     * @param string $topic_icons_table The topic_icons table name
+     */
+    public function __construct(\phpbb\cache\service $cache, \phpbb\config\config $config, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, string $root_path, string $topic_icons_table)
+    {
+        $this->cache = $cache;
+        $this->config = $config;
+        $this->template = $template;
+        $this->root_path = $root_path;
+        $this->topic_icons_table = $topic_icons_table;
+        $this->db = $db;
+    }
+
+    /**
+     * Assign icons to template
+     *
+     * @param int $topic_id The topic ID for which to assign icons to tempalte
+     * @param int $icon_id Topic's icon_id assignment
+     */
+    public function assign_icons_to_template(int $topic_id, int $icon_id = 0)
+    {
+        $topic_icons = [];
+        if ($topic_id)
+        {
+            $sql = "SELECT icon_id FROM {$this->topic_icons_table} WHERE topic_id = $topic_id";
+            $result = $this->db->sql_query($sql);
+            $topic_icons = $this->db->sql_fetchrowset($result);
+            $this->db->sql_freeresult($result);
+        }
+
+        $root_path = (defined('PHPBB_USE_BOARD_URL_PATH') && PHPBB_USE_BOARD_URL_PATH) ? generate_board_url() . '/' : $this->root_path;
+        $icons = $this->cache->obtain_icons();
+        foreach ($icons as $id => $data)
+        {
+            if ($data['display'])
+            {
+                $topic_icon = array_filter($topic_icons, function ($icon) use ($id)
+                {
+                    return $icon['icon_id'] == $id;
+                });
+                $this->template->assign_block_vars('topic_icons', [
+                    'ICON_ID' => $id,
+                    'ICON_IMG' => $root_path . $this->config['icons_path'] . '/' . $data['img'],
+                    'ICON_WIDTH' => $data['width'],
+                    'ICON_HEIGHT' => $data['height'],
+                    'ICON_ALT' => $data['alt'],
+                    'S_CHECKED' => !empty($topic_icon) || $id === $icon_id,
+                ]);
+            }
+        }
+    }
+
+    public function save_topic_icons(int $topic_id, array $topic_icons)
+    {
+        $sql = "DELETE FROM {$this->topic_icons_table} WHERE topic_id = $topic_id";
+        $this->db->sql_query($sql);
+
+        $sql_ary = [];
+        foreach ($topic_icons as $icon_id)
+        {
+            $sql_ary[] = [
+                'topic_id' => $topic_id,
+                'icon_id' => (int)$icon_id,
+            ];
+        }
+        if (count($sql_ary))
+        {
+            $this->db->sql_multi_insert($this->topic_icons_table, $sql_ary);
+        }
+    }
 }
